6de6e6911d448ef41ea3726d65f4e889fa7d580b
Oleg Shalygin <oshalygin@gmail.com>
2015-12-19 22:16:34 -0800

    added some databinding

---
+++ application/jedis.component.ts
@@ -0,0 +1,25 @@
+import {Component} from "angular2/core"
+import {bootstrap} from "angular2/platform/browser"
+
+@Component({
+    selector: "jedis",
+    template: `
+    <div>
+    <ul>
+    <li *ngFor="#jedi of jedis">{{jedi}}</li>
+    </ul>
+</div>
+`
+})
+
+export class Jedi {
+    jedis: string[]
+    /**
+     *
+     */
+    constructor() {
+        this.jedis = ["Luke", "Darth Vader", "Obiwan", "Yoda", "Anakin"];
+    }
+}
+
+bootstrap(Jedi);
