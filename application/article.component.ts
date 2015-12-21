80186f010889d569cd6ca7e6e7e67debb92ee788
Oleg Shalygin <oshalygin@gmail.com>
2015-12-20 18:22:53 -0800

    cleaning out the articles template

--- application/article.component.ts
+++ application/article.component.ts
@@ -9,17 +9,26 @@ import {bootstrap} from "angular2/platform/browser"
     template: `
             <div class="panel panel-primary">
                 <div class="panel-heading text-center">
-                    Article
+                   <a href="{{ link }}"> Article </a>
+                   <span class="badge"> 4</span>
                 </div>
                 <div class="panel-body">
-                    Stuff about the Article
-
+                <div class="row">
+                    <h3>{{ title }}</h3>
+                    <p> {{ shortLink }} </p>
+                </div>
+                <div class="row">
+                    <button type="button" class="btn btn-primary col-md-offset-2 col-md-3" (click)="voteDown()"><i class="glyphicon glyphicon-chevron-up"></i> Up</button>
+                    <button type="button" class="btn btn-danger col-md-offset-2 col-md-3" (click)="voteDown()"><i class="glyphicon glyphicon-chevron-down"></i> Down</button>
+                </div>
                 </div>
             </div>
     `
 })
 
 export class Article {
+    voteCount: number;
+
 
 }
 
